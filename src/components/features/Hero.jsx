fe8bb1752eaff3c86549a8c2ad04b2f36ff38929
Dashunya Sharova <dashunya@DMac.local>
2025-03-20 20:37:22 -0600

    feat: add hero section conponent and layout

---
+++ src/components/features/Hero.jsx
@@ -0,0 +1,19 @@
+import '../../css/components/hero.css'
+
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="container">
+        <h1>Build The Community Your Fans Will Love</h1>
+        <p>
+          Huddle re-imagines the way we build communities. You have a voice, but
+          so does your audience. Create connections with your users as you
+          engage in genuine discussion.
+        </p>
+        <button className="cta-btn btn-primary">get scarced For free</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
