238c15cc44e67c6a3efa2666db35b2b3f01c1e7e
Dashunya Sharova <dashunya@DMac.local>
2025-03-20 19:55:25 -0600

    style: added basic styles and resets

--- src/components/globals/Header.jsx
+++ src/components/globals/Header.jsx
@@ -2,9 +2,9 @@ import logo from "/src/assets/images/logo.svg";
 
 function Header() {
   return (
-    <header>
-      <nav aria-label="Main Navigation">
-        <img src={logo} alt="Company Logo" width="150"  />
+    <header className="header">
+      <nav aria-label="Main Navigation" className="nav main-nav">
+        <img src={logo} alt="Company Logo" width="150" className="logo" />
         <ul>
           <li>
             <button className="cta-btn">Try it Free</button>
