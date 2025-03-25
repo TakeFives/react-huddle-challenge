808998eb3f4612beb8e63d645133514768476e18
Dashunya Sharova <dashunya@DMac.local>
2025-03-24 19:24:35 -0600

    feat: refactor hero and add feature list

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/globals/Header";
 
 import "./App.css";
 import Hero from "./components/features/Hero";
+import FeatureList from "./components/features/FeatureList";
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       <Header />
       <main>
         <Hero />
-        <section className="feature-list"></section>
+        <section className="feature-list">
+          <FeatureList />
+        </section>
         <section className="cta-floating-section"></section>
       </main>
       <footer></footer>
--- src/components/features/Hero.jsx
+++ src/components/features/Hero.jsx
@@ -1,16 +1,21 @@
-import '../../css/components/hero.css'
+import "../../css/components/hero.css";
 
 function Hero() {
   return (
     <section className="hero">
-      <div className="container">
-        <h1>Build The Community Your Fans Will Love</h1>
-        <p>
-          Huddle re-imagines the way we build communities. You have a voice, but
-          so does your audience. Create connections with your users as you
-          engage in genuine discussion.
-        </p>
-        <button className="cta-btn btn-primary">get scarced For free</button>
+      <div className="container hero__container">
+        <div className="hero__info">
+          <h1>Build The Community Your Fans Will Love</h1>
+          <p>
+            Huddle re-imagines the way we build communities. You have a voice,
+            but so does your audience. Create connections with your users as you
+            engage in genuine discussion.
+          </p>
+          <button className="btn-primary hero__cta">get scarced For free</button>
+        </div>
+        <div className="hero__image">
+          <img src="/src/assets/images/illustration-mockups.svg" alt="Hero image artwork of product charts"/>
+        </div>
       </div>
     </section>
   );
